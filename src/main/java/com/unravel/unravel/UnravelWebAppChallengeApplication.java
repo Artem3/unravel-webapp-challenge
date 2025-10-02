939e9fe01d3b2ae418068c672910c86074a2863d
Artem3 <artem.goncharenko@gmail.com>
2025-10-02 18:15:26 +0200

    Initial project setup with Maven Wrapper

---
+++ src/main/java/com/unravel/unravel/UnravelWebAppChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.unravel.unravel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnravelWebAppChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnravelWebAppChallengeApplication.class, args);
+	}
+
+}
