4a4c8e7a68ef86632d8557b5a7f912cd5246aa0f
Artem3 <artem.goncharenko@gmail.com>
2025-10-04 17:06:43 +0200

    TSK-2. Fix memory leaks. Refactor 'largeSessionData' by using Caffeine. Add logs

--- src/main/java/com/unravel/part2MemoryManagemet/MemoryManager.java
+++ src/main/java/com/unravel/part2MemoryManagemet/MemoryManager.java
@@ -1,16 +1,27 @@
 package com.unravel.part2MemoryManagemet;
 
-import java.util.HashMap;
-import java.util.Map;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public class MemoryManager {
-    private static Map<String, byte[]> largeSessionData = new HashMap<>();
+
+    private static final Cache<String, byte[]> largeSessionData = Caffeine.newBuilder()
+            .maximumSize(500)  // limit to 500 entries (adjust based on heap size)
+            .expireAfterAccess(10, TimeUnit.SECONDS)  // Optionally. Evict after 10 sec inactivity
+            .weakKeys()       // Optionally. Allow GC to collect if keys are unreferenced
+            .build();
 
     public static void addSessionData(String sessionId) {
-        largeSessionData.put(sessionId, new byte[10 * 1024 * 1024]); // 10MB persession
+        largeSessionData.put(sessionId, new byte[10 * 1024 * 1024]);  // 10MB
     }
 
+    // call this method when a session is invalidated or logged out to explicitly remove data
     public static void removeSessionData(String sessionId) {
-        largeSessionData.remove(sessionId);
+        log.info("Removing session data for ID: {}", sessionId);
+        largeSessionData.invalidate(sessionId);
     }
 }
