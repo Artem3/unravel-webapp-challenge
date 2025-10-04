a246015509d91838467e22cb5d21396c6db9e77f
Artem3 <artem.goncharenko@gmail.com>
2025-10-04 17:07:41 +0200

    TSK-2. Add logs and exception to the TSK-1

--- src/main/java/com/unravel/part1SessionManagement/SessionManager.java
+++ src/main/java/com/unravel/part1SessionManagement/SessionManager.java
@@ -1,10 +1,12 @@
 package com.unravel.part1SessionManagement;
 
 import jakarta.servlet.http.HttpSession;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
 
+@Slf4j
 @Service
 public class SessionManager {
 
@@ -14,19 +16,30 @@ public class SessionManager {
         }
         String sessionId = session.getId();
         session.setAttribute("userId", userId);
+        log.info("User {} logged in with session ID: {}", userId, session.getId());
+
         return "Login successful. Session ID: " + sessionId;
     }
 
     public String logout(HttpSession session) {
+        String sessionId = session.getId();
         session.invalidate();
+        log.info("Logout successful for session ID: {}", sessionId);
+
         return "Logout successful.";
     }
 
     public String getSessionDetails(HttpSession session) {
         String userId = (String) session.getAttribute("userId");
         if (userId == null) {
-            throw new RuntimeException("Session not found");
+            throw new CustomSessionException("Session not found");
         }
         return "Session ID for user " + userId + ": " + session.getId();
+    }
+
+    class CustomSessionException extends RuntimeException {
+        public CustomSessionException(String message) {
+            super(message);
+        }
     }
 }
